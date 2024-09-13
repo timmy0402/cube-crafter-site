e3c0a35646edc65f3483d677681e2b0535479f6f
timmy0402 <minhnhathb04@gmail.com>
2024-09-13 12:01:52 -0700

    navbar setup

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-left">
+                <a href="/" className="logo">
+                    Cube Crafter
+                </a>
+            </div>
+            <div className="nav-right">
+
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
+

eae5540b9360544c40750544088330e1a18662b5
timmy0402 <145733724+timmy0402@users.noreply.github.com>
2024-09-16 19:01:27 +0000

    feature page

--- src/pages/features.js
+++ src/pages/features.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
+import "../styles/Features.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Features = () => {
     return (
-        <div>
-            <h1>Features</h1>
-            <p>Get in touch with us.</p>
-        </div>
+        <>
+            <Navbar />
+
+            <div className="feature">
+                <h1 className="title">Features</h1>
+                <h2 className="header">Create Scramble</h2>
+                <p className="description">
+                    Scramble for all WCA puzzles are supported using csTimer
+                    (WCA Official Scrambler) algorithms.
+                </p>
+
+                <h2 className="header">Image Generation</h2>
+                <p className="description">
+                    Transform your solving experience with our innovativeBot
+                    create a detailed image of each face of your cube,
+                    accurately reflecting the scramble. This tool ensures you
+                    can easily verify and track your scramble, making practice
+                    and competition preparation more precise and efficient than
+                    ever before.
+                </p>
+                <p className="descrption">
+                    <strong>Supported puzzle:</strong> 3x3
+                </p>
+                <h2 className="header">Timer</h2>
+                <p className="description">
+                    Elevate your solving sessions with our built-in timer
+                    feature. Designed for speed cubers of all levels, it allows
+                    you to accurately measure your solving time with precision.
+                </p>
+                <p className="description">
+                    <strong>Comming: </strong> Track time and average time
+                </p>
+            </div>
+
+            <Footer />
+        </>
     );
 };
 

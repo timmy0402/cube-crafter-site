d3b2513260e74fdde41148032626c48a9fab0efa
timmy0402 <minhnhathb04@gmail.com>
2024-09-26 18:16:25 -0700

    added new discord support channel

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -12,6 +12,10 @@ const Contact = () => {
                 </p>
                 <h2 className="header">Email</h2>
                 <p>cubecraftersdiscordcontact@gmail.com</p>
+                <h2 className="header">Discord Support Server</h2>
+                <a href="https://discord.gg/sq4Qa9vavc" className="btn">
+                    Join us
+                </a>
             </div>
         </>
     );

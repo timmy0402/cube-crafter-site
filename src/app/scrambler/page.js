a88ba9c283a4a713e352c07c662bceed6d759a87
timmy0402 <145733724+timmy0402@users.noreply.github.com>
2024-10-01 23:27:26 +0000

    added scrambler page

---
+++ src/app/scrambler/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Scrambler() {
+  return (
+    <>
+      <div className="scrambler">
+        <h1 className="header">Scramble Generator</h1>
+      </div>
+    </>
+  );
+}

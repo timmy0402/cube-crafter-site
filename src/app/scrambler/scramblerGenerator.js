836d2d2ac0b722c6d3f4943f98c2c971ac0cf03d
timmy0402 <145733724+timmy0402@users.noreply.github.com>
2024-10-02 17:55:38 +0000

    try to put type : module in package.json

---
+++ src/app/scrambler/scramblerGenerator.js
@@ -0,0 +1,6 @@
+//import { randomScrambleForEvent } from "https://cdn.cubing.net/v0/js/cubing/scramble";
+
+const createScramble = () => {
+  return  3//randomScrambleForEvent("333"));
+};
+console.log(createScramble());

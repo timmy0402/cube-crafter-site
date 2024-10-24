51e0264dd605bba1aeb231d418dd2419de3bf0d3
timmy0402 <145733724+timmy0402@users.noreply.github.com>
2024-10-24 03:04:57 +0000

    try to add 2 values for each options

--- src/app/scrambler/page.js
+++ src/app/scrambler/page.js
@@ -1,9 +1,9 @@
 "use client";
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "../../styles/Scrambler.css";
 import { randomScrambleForEvent } from "cubing/scramble";
-import { TwistyPlayer } from "cubing/twisty";
+import "cubing/twisty";
 
 async function createScramble(puzzle) {
   const scramble = await randomScrambleForEvent(puzzle);
@@ -13,12 +13,20 @@ async function createScramble(puzzle) {
 export default function Scrambler() {
   const [puzzle, setPuzzle] = React.useState("");
   const [scramble, setScramble] = React.useState("");
+  const playerRef2d = useRef(null);
+  const playerRef3d = useRef(null);
 
   const handleChange = async (event) => {
-    const newPuzzle = event.target.value;
+    const newPuzzle = event.target.value.scramble;
     setPuzzle(newPuzzle);
     const newScramble = await createScramble(newPuzzle);
     setScramble(newScramble);
+    if (playerRef2d.current) {
+      playerRef2d.current.alg = newScramble;
+    }
+    if (playerRef3d.current) {
+      playerRef3d.current.alg = newScramble;
+    }
   };
   return (
     <>
@@ -28,20 +36,42 @@ export default function Scrambler() {
           <label>
             Puzzle:
             <select value={puzzle} onChange={handleChange}>
-              <option value="222">2x2</option>
-              <option value="333">3x3</option>
-              <option value="444">4x4</option>
-              <option value="555">5x5</option>
-              <option value="666">6x6</option>
-              <option value="777">7x7</option>
-              <option value="sq1">Square1</option>
-              <option value="skewb">Skewb</option>
-              <option value="minx">Megaminx</option>
-              <option value="pyram">Pyraminx</option>
-              <option value="clock">Clock</option>
+              <option value='{"scramble":"222","visual":"2x2x2"}'>2x2</option>
+              <option value='{"scramble":"333","visual":"3x3x3"}'>3x3</option>
+              <option value='{"scramble":"444","visual":"4x4x4"}'>4x4</option>
+              <option value='{"scramble":"555","visual":"5x5x5"}'>5x5</option>
+              <option value='{"scramble":"666","visual":"6x6x6"}'>6x6</option>
+              <option value='{"scramble":"777","visual":"7x7x7"}'>7x7</option>
+              <option value='{"scramble":"sq1","visual":"square1"}'>
+                Square1
+              </option>
+              <option value='{"scramble":"skewb","visual":"skewb"}'>
+                Skewb
+              </option>
+              <option value='{"scramble":"mix","visual":"megaminx"}'>
+                Megaminx
+              </option>
+              <option value='{"scramble":"pyram","visual":"pyraminx"}'>
+                Pyraminx
+              </option>
+              <option value='{"scramble":"clock","visual":"clock"}'>
+                Clock
+              </option>
             </select>
           </label>
-          <div>Scramble: {scramble}</div>
+          <div>
+            Scramble: {scramble}
+            <twisty-player
+              ref={playerRef2d}
+              visualization="2D"
+              background="none"
+            ></twisty-player>{" "}
+            <twisty-player
+              ref={playerRef3d}
+              visualization="3D"
+              background="none"
+            ></twisty-player>{" "}
+          </div>
         </div>
       </div>
     </>

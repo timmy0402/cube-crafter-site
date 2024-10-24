9372577d8af742334fbe88514f8ef912f96ccdcc
timmy0402 <145733724+timmy0402@users.noreply.github.com>
2024-10-24 04:38:39 +0000

    added toggle 3d

--- src/app/scrambler/page.js
+++ src/app/scrambler/page.js
@@ -11,20 +11,42 @@ async function createScramble(puzzle) {
 }
 
 export default function Scrambler() {
-  const [puzzle, setPuzzle] = React.useState("");
+  const [puzzle, setPuzzle] = React.useState(
+    '{"scramble":"333","visual":"3x3x3"}',
+  );
   const [scramble, setScramble] = React.useState("");
+  const [cube, setCube] = React.useState("");
+  const [is3D, setIs3D] = React.useState(false);
+
   const playerRef2d = useRef(null);
   const playerRef3d = useRef(null);
 
+  const handleCheckboxChange = () => {
+    setIs3D(!is3D);
+    const value = JSON.parse(puzzle);
+    if (!is3D) {
+      playerRef2d.current.puzzle = value.visual;
+      playerRef2d.current.alg = scramble;
+    } else {
+      playerRef3d.current.puzzle = value.visual;
+      playerRef3d.current.alg = scramble;
+    }
+  };
+
   const handleChange = async (event) => {
-    const newPuzzle = event.target.value.scramble;
-    setPuzzle(newPuzzle);
+    const value = event.target.value;
+    const parsedValue = JSON.parse(value);
+    const newPuzzle = parsedValue.scramble;
+    setCube(parsedValue.visual);
+    setPuzzle(value);
     const newScramble = await createScramble(newPuzzle);
     setScramble(newScramble);
     if (playerRef2d.current) {
+      playerRef2d.current.puzzle = parsedValue.visual;
       playerRef2d.current.alg = newScramble;
     }
     if (playerRef3d.current) {
+      playerRef3d.current.puzzle = parsedValue.visual;
       playerRef3d.current.alg = newScramble;
     }
   };
@@ -48,7 +70,7 @@ export default function Scrambler() {
               <option value='{"scramble":"skewb","visual":"skewb"}'>
                 Skewb
               </option>
-              <option value='{"scramble":"mix","visual":"megaminx"}'>
+              <option value='{"scramble":"minx","visual":"megaminx"}'>
                 Megaminx
               </option>
               <option value='{"scramble":"pyram","visual":"pyraminx"}'>
@@ -59,18 +81,39 @@ export default function Scrambler() {
               </option>
             </select>
           </label>
+          <p className="3dLabel">3D</p>
+          <label className="switch">
+            <input
+              type="checkbox"
+              checked={is3D}
+              onChange={handleCheckboxChange}
+            />
+            <span className="slider round"></span>
+          </label>
           <div>
             Scramble: {scramble}
-            <twisty-player
-              ref={playerRef2d}
-              visualization="2D"
-              background="none"
-            ></twisty-player>{" "}
-            <twisty-player
-              ref={playerRef3d}
-              visualization="3D"
-              background="none"
-            ></twisty-player>{" "}
+            {!is3D && (
+              <div className="scramble-container">
+                <twisty-player
+                  ref={playerRef2d}
+                  visualization="2D"
+                  background="none"
+                  alg={scramble}
+                  puzzle={cube}
+                ></twisty-player>{" "}
+              </div>
+            )}
+            {is3D && (
+              <div className="scramble-container">
+                <twisty-player
+                  ref={playerRef3d}
+                  visualization="3D"
+                  background="none"
+                  alg={scramble}
+                  puzzle={cube}
+                ></twisty-player>{" "}
+              </div>
+            )}
           </div>
         </div>
       </div>

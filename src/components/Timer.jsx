9ecbf1d87712ca90bc8d28fa86654a62b1603a5e
timmy0402 <145733724+timmy0402@users.noreply.github.com>
2024-11-07 04:02:28 +0000

    Final timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,45 +4,59 @@ import { useState, useRef } from "react";
 export default function Timer() {
     const [time, setTime] = React.useState(0);
     const [isRunning, setIsRunning] = React.useState(false);
-    const [timerID,setTimerID] = React.useState(null);
+    const [isReady, setIsReady] = React.useState(false);
+    const [timerID, setTimerID] = React.useState(null);
 
     const startTimer = () => {
-        if(!isRunning){
+        if (!isRunning) {
             setIsRunning(true);
             const id = setInterval(() => {
                 setTime((prevTime) => prevTime + 1);
-            },10);
+            }, 10);
             setTimerID(id);
         }
     };
 
     const stopTimer = () => {
-        if(isRunning){
+        if (isRunning) {
             clearInterval(timerID);
             setIsRunning(false);
         }
-    }
+    };
     useEffect(() => {
         const handleKeyPress = (event) => {
-          if (event.code === 'Space') {
-            event.preventDefault(); // Prevent default spacebar scroll behavior
-            if (isRunning) {
-              stopTimer();
-            } else {
-              startTimer();
+            if (event.code === "Space") {
+                event.preventDefault(); // Prevent default spacebar scroll behavior
+                console.log("press ready: " + isReady);
+                console.log("press running: " + isRunning);
+                if (!isRunning && !isReady) {
+                    setIsReady(true);
+                    setTime(0);
+                } else if (isRunning && !isReady) {
+                    stopTimer();
+                }
             }
-          }
         };
-    
+        const handleKeyUp = (event) => {
+            if (event.code === "Space") {
+                if (isReady && !isRunning) {
+                    startTimer();
+                    setIsReady(false);
+                }
+            }
+        };
+
         // Attach event listener
-        document.addEventListener('keydown', handleKeyPress);
-    
+        document.addEventListener("keydown", handleKeyPress);
+        document.addEventListener("keyup", handleKeyUp);
+
         // Cleanup event listener on component unmount
         return () => {
-          document.removeEventListener('keydown', handleKeyPress);
-          if (timerID) clearInterval(timerID);
+            document.removeEventListener("keydown", handleKeyPress);
+            document.removeEventListener("keyup", handleKeyUp);
+            if (timerID) clearInterval(timerID);
         };
-      }, [isRunning, timerID]);
+    }, [isRunning, isReady, timerID]);
 
     // Minutes calculation
     const minutes = Math.floor((time % 360000) / 6000);
@@ -53,24 +67,6 @@ export default function Timer() {
     // Milliseconds calculation
     const milliseconds = time % 100;
 
-    // Method to reset timer back to 0
-    const reset = () => {
-        setTime(0);
-    };
-
-    const handleKeyUp = (event) => {
-        if (event.key === " ") {
-            console.log("key up");
-        }
-    };
-
-    const handleKeyDown = (event) => {
-        if (event.key === " ") {
-            console.log("key down");
-        }
-    };
-
-
     return (
         <>
             <div className="timer">
@@ -79,6 +75,12 @@ export default function Timer() {
                     {seconds.toString().padStart(2, "0")}:
                     {milliseconds.toString().padStart(2, "0")}
                 </p>
+            </div>
+            <div className="average">
+                <div className="ao5">
+                    <p></p>
+                </div>
+                <div className="ao10"></div>"
             </div>
         </>
     );

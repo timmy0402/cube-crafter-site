21f102616c9c714c78a4d718148d80976f2c36dd
timmy0402 <145733724+timmy0402@users.noreply.github.com>
2026-05-12 14:43:24 -0500

    update site map

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -15,6 +15,12 @@ export default function sitemap() {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/commands`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
